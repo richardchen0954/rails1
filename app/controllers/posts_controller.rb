f956cde12aa2173b5fbb8061da3c9a7af525a157
西影行 <richardchen0954@gmail.com>
2017-06-08 23:46:19 +0800

    临时

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new,:create]
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post =
 end
